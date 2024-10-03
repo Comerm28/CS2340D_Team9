96704a8eb5827eaf9bbf1de4e40f38d449fd2f4d
Ash <programmingfrog@gmail.com>
2024-10-02 22:01:22 -0400

    added authentication

--- Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/view/CreateAccountActivity.java
+++ Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/view/CreateAccountActivity.java
@@ -2,15 +2,17 @@ package com.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.sprintproject.R;
-import com.viewmodel.LoginViewModel;
+import com.google.android.material.snackbar.Snackbar;
+import com.viewmodel.AuthenticationViewModel;
 
 public class CreateAccountActivity extends AppCompatActivity {
-    private LoginViewModel loginViewModel;
+    private AuthenticationViewModel authenticationViewModel;
     private EditText createUsernameEditText;
     private EditText createPasswordEditText;
 
@@ -19,7 +21,7 @@ public class CreateAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account); // Reference to your create account layout
 
-        loginViewModel = new LoginViewModel();
+        authenticationViewModel = new AuthenticationViewModel();
         createUsernameEditText = findViewById(R.id.createUsernameEditText);
         createPasswordEditText = findViewById(R.id.createPasswordEditText);
         Button registerButton = findViewById(R.id.registerButton);
@@ -27,26 +29,21 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         // Handle register button click
         registerButton.setOnClickListener(view -> {
-            // Logic for registration can be added here
             String username = createUsernameEditText.getText().toString();
             String password = createPasswordEditText.getText().toString();
-            if(loginViewModel.onCreateAccount(username, password)){
-                //Navigate back to the login page
-                Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            authenticationViewModel.signUp(username, password, () -> {
+                //todo: maybe bundle user name and password and autofill them into the edittext on login screen?
+                Intent intent = new Intent(this, LoginActivity.class);
                 startActivity(intent);
-                //todo: maybe bundle user name and passowrd and autofill them into the edittext on login screen?
-            }
-            else{
-                //todo: if account has already been made with this username, maybe add bundle with just username when sending back to login screen
-                //or just general error handling like white space and stuff, or like empty strings
-            }
-
+            }, fail -> Snackbar.make(view, fail, Snackbar.LENGTH_LONG).show());
         });
 
         // Handle login button click
         loginButton.setOnClickListener(view -> {
-            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
-            startActivity(intent);
+            String username = createUsernameEditText.getText().toString();
+            String password = createPasswordEditText.getText().toString();
+            authenticationViewModel.logIn(username, password, () -> startActivity(new Intent(this, MainAppScreensActivity.class)), fail -> Snackbar.make(view, fail, Snackbar.LENGTH_LONG).show());
         });
     }
+
 }
