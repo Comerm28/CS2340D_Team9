15071573f99c39e6e8e7d7d9f5fe6a522b70c935
Tyler Vo <tvo@Tylers-MacBook-Pro-2.local>
2024-10-15 19:43:00 -0400

    task and teammember code

--- Sprint0.5_Fa24-main/SOLID_GRASP/TeamMember.java
+++ Sprint0.5_Fa24-main/SOLID_GRASP/TeamMember.java
@@ -2,13 +2,22 @@ public abstract class TeamMember {
     protected String name;
     protected String email;
 
-    //TODO: make super constructor actually set the variables
-    public TeamMember()
-    {
-        name = "";
-        email = "";
+    // Constructor to initialize name and email
+    public TeamMember(String name, String email) {
+        this.name = name;
+        this.email = email;
     }
 
-    public abstract boolean join_project();
-    public abstract boolean leave_project();
+    // Abstract methods for joining and leaving a project
+    public abstract boolean joinProject(Project project);
+    public abstract boolean leaveProject(Project project);
+
+    // Getters for name and email
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
