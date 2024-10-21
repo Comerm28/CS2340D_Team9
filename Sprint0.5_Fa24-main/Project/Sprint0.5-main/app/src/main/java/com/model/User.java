3d728f0d6cd88e8d3e5c0a346abc35645a8073f3
Ash <programmingfrog@gmail.com>
2024-10-21 15:13:37 -0400

    added database singleton

--- Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/model/User.java
+++ Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/model/User.java
@@ -1,10 +1,15 @@
 package com.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String username;
+    private List<Destination> destinations;
 
     public User(String username) {
         this.username = username;
+        this.destinations = new ArrayList<>();
     }
 
     public static String formatEmail(String username) {
@@ -13,6 +18,10 @@ public class User {
 
     public String getUsername() {
         return username;
+    }
+
+    public List<Destination> getDestinations() {
+        return destinations;
     }
 
     public void setUsername(String username) {
--- Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/viewmodel/AuthenticationViewModel.java
+++ Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/viewmodel/AuthenticationViewModel.java
@@ -5,22 +5,13 @@ import androidx.lifecycle.ViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.model.Database;
 import com.model.User;
 
 import java.util.function.Consumer;
 
 public class AuthenticationViewModel extends ViewModel {
-    private final DatabaseReference dbRef;
-    private final FirebaseAuth mAuth;
-
-    // initialize viewmodel
-    public AuthenticationViewModel() {
-        dbRef = FirebaseDatabase.getInstance().getReference();
-        mAuth = FirebaseAuth.getInstance();
-    }
-
-    public void signUp(String username, String password, Runnable onSuccess,
-                       Consumer<String> onFail) {
+    public void signUp(String username, String password, Runnable onSuccess, Consumer<String> onFail) {
         // validate login info format
         if (!isValidLogin(username, password)) {
             onFail.accept("Login info improperly formatted. "
@@ -28,19 +19,10 @@ public class AuthenticationViewModel extends ViewModel {
             return;
         }
 
-        // attempt to create the user, and if successful, call onSuccess,
-        // else call onFail with the error message
-        mAuth.createUserWithEmailAndPassword(User.formatEmail(username), password)
-                .addOnSuccessListener(task -> {
-                    dbRef.child("users").child(username).setValue(new User(username));
-                    onSuccess.run();
-                }).addOnFailureListener(fail ->
-                        onFail.accept(fail.getMessage().replace(
-                                "email address", "username")));
+        Database.getInstance().signUp(User.formatEmail(username), password, onSuccess, onFail);
     }
 
-    public void logIn(String username, String password,
-                      Runnable onSuccess, Consumer<String> onFail) {
+    public void logIn(String username, String password, Runnable onSuccess, Consumer<String> onFail) {
         // validate login info format
         if (!isValidLogin(username, password)) {
             onFail.accept("Login info improperly formatted. "
@@ -48,12 +30,7 @@ public class AuthenticationViewModel extends ViewModel {
             return;
         }
 
-        // attempt to create the user, and if successful, call onSuccess,
-        // else call onFail with the error message
-        mAuth.signInWithEmailAndPassword(User.formatEmail(username), password)
-                .addOnSuccessListener(task -> onSuccess.run())
-                .addOnFailureListener(fail -> onFail.accept(fail.getMessage().
-                        replace("email address", "username")));
+        Database.getInstance().logIn(User.formatEmail(username), password, onSuccess, onFail);
     }
 
 
