fda53e6bc91a58d013646fde6fbc42bc2a9c8f91
Tyler Vo <tvo@Tylers-MacBook-Pro-2.local>
2024-10-22 18:41:35 -0400

    typo in user

--- Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/model/User.java
+++ Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/model/User.java
@@ -1,15 +1,10 @@
 package com.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class User {
     private String username;
-    private List<Destination> destinations;
 
     public User(String username) {
         this.username = username;
-        this.destinations = new ArrayList<>();
     }
 
     public static String formatEmail(String username) {
@@ -19,14 +14,6 @@ public class User {
     public String getUsername() {
         return username;
     }
-<<<<<<< HEAD
-=======
-
-    public List<Destination> getDestinations() {
-        return destinations;
-    }
-
->>>>>>> main
     public void setUsername(String username) {
         this.username = username;
     }
