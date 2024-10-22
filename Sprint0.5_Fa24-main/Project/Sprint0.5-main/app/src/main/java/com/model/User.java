f7d223f33134c322d924e6cb3bed71b23a762e81
Ash <programmingfrog@gmail.com>
2024-10-22 19:07:08 -0400

    fixed login problem and removed destinations from user

--- Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/model/User.java
+++ Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/model/User.java
@@ -5,11 +5,9 @@ import java.util.List;
 
 public class User {
     private String username;
-    private List<Destination> destinations;
 
     public User(String username) {
         this.username = username;
-        this.destinations = new ArrayList<>();
     }
 
     public static String formatEmail(String username) {
@@ -19,16 +17,8 @@ public class User {
     public String getUsername() {
         return username;
     }
-<<<<<<< HEAD
-=======
 
-    public List<Destination> getDestinations() {
-        return destinations;
-    }
-
->>>>>>> main
     public void setUsername(String username) {
         this.username = username;
     }
-
 }
