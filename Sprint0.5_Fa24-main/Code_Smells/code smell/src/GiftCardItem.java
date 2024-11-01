498e1feef7c9aab5791db68a03138082ba67dd91
Yu Lu <114430869+ylinlu6220@users.noreply.github.com>
2024-11-01 13:35:08 -0400

    Deleted GiftCardItem Class
    Deleted Lazy class

--- Sprint0.5_Fa24-main/Code_Smells/code smell/src/GiftCardItem.java
+++ Sprint0.5_Fa24-main/Code_Smells/code smell/src/GiftCardItem.java
@@ -1,7 +1,8 @@
-
+/* Lazy/Data class: To resolve, delete class, use item class instead
 public class GiftCardItem extends Item {
     public GiftCardItem(String name, double price, int quantity, DiscountType discountType, double discountAmount){
         super(name, price, quantity, discountType, discountAmount);
     }
 
 }
+*/
