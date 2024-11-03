e5442fbb2a091ff70ab4a04a5f742a0fa4e0dc4c
Ash <programmingfrog@gmail.com>
2024-11-02 21:10:11 -0400

    cleaned up order by removing email/gift card methods and cleaned up the calculate total cost by delegating it to the item class

--- Sprint0.5_Fa24-main/Code_Smells/code smell/src/TaxableItem.java
+++ Sprint0.5_Fa24-main/Code_Smells/code smell/src/TaxableItem.java
@@ -1,13 +1,14 @@
 
 public class TaxableItem extends Item {
     private double taxRate;
-    
-    public TaxableItem(String name, double price, int quantity, DiscountType discountType, double discountAmount, double taxRate){
+
+    public TaxableItem(String name, double price, int quantity, DiscountType discountType, double discountAmount,
+            double taxRate) {
         super(name, price, quantity, discountType, discountAmount);
         this.taxRate = taxRate;
     }
 
-    public double getTaxRate(){
+    public double getTaxRate() {
         return taxRate;
     }
 
@@ -15,4 +16,43 @@ public class TaxableItem extends Item {
     public double calculateDiscountedPrice() {
         return (1 + (taxRate / 100.0)) * super.getPrice();
     }
+
+    @Override
+    public double getNetCost() {
+        double cost = price;
+        switch (discountType) {
+            case PERCENTAGE:
+                cost -= discountAmount * price;
+                break;
+            case AMOUNT:
+                cost -= discountAmount;
+                break;
+        }
+
+        cost += taxRate * price;
+
+        return cost * quantity;
+    }
+
+    // double total = 0.0;
+    // for (Item item : items) {
+    // double price = item.getPrice();
+    // switch (item.getDiscountType()) {
+    // case PERCENTAGE:
+    // price -= item.getDiscountAmount() * price;
+    // break;
+    // case AMOUNT:
+    // price -= item.getDiscountAmount();
+    // break;
+    // default:
+    // // no discount
+    // break;
+    // }
+    // total += price * item.getQuantity();
+    // if (item instanceof TaxableItem) {
+    // TaxableItem taxableItem = (TaxableItem) item;
+    // double tax = taxableItem.getTaxRate() / 100.0 * item.getPrice();
+    // total += tax;
+    // }
+    // }
 }
