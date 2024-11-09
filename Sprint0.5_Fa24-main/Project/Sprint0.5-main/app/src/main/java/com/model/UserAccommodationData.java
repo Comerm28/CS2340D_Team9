d1a5a9d7421a89e01e6fe3e5d53bc57251fec6bf
ndave24 <ndave1415@gmail.com>
2024-11-09 17:59:03 -0500

    Fixed typos in CurrentUserInfo which prevented app from running, UserAccommodationData now only has list of AccommodationReservations

--- Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/model/UserAccommodationData.java
+++ Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/model/UserAccommodationData.java
@@ -1,70 +1,34 @@
 package com.model;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserAccommodationData {
     private String username;
-    private Date checkInTime;
-    private Date checkOutTime;
-    private String location;
-    private int numberOfRooms;
-    private RoomType roomType;
+    private List<AccommodationReservation> reservations;
 
-    public UserAccommodationData() {}
+    public UserAccommodationData() {
+        reservations = new ArrayList<>();
+    }
 
     public UserAccommodationData(String username){
         username = username;
+        reservations = new ArrayList<>();
     }
 
-    public Date getCheckInTime() {
-        return checkInTime;
+    public String getUsername() {
+        return username;
     }
 
-    public void setCheckInTime(Date checkInTime) {
-       this.checkInTime = checkInTime;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public Date getCheckOutTime() {
-        return checkOutTime;
+    public void addReservation(AccommodationReservation reservation) {
+        reservations.add(reservation);
     }
 
-    public void setCheckOutTime(Date checkOutTime) {
-        this.checkOutTime = checkOutTime;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public int getNumberOfRooms() {
-        return numberOfRooms;
-    }
-
-    public void setNumberOfRooms(int numberOfRooms) {
-        this.numberOfRooms = numberOfRooms;
-    }
-
-    public RoomType getRoomType() {
-        return roomType;
-    }
-
-    public void setRoomType(RoomType roomType) {
-        this.roomType = roomType;
-    }
-
-    public enum RoomType {
-        Single("Single"),
-        KingSuite("King Suite"),
-        Deluxe("Deluxe");
-
-        public final String displayString;
-
-        RoomType(String displayString) {
-            this.displayString = displayString;
-        }
+    public List<AccommodationReservation> getReservations() {
+        return reservations;
     }
 }
